a81b9f37e8a171160e50aefca1a7208b3ddb5a96
Mara <mara.graziani@ibm.com>
2024-06-05 09:01:18 +0200

    Fix read test

--- service/routes.py
+++ service/routes.py
@@ -89,7 +89,7 @@ def create_products():
     #
     # Uncomment this line of code once you implement READ A PRODUCT
     #
-    # location_url = url_for("get_products", product_id=product.id, _external=True)
+    location_url = url_for("get_products", product_id=product.id, _external=True)
     location_url = "/"  # delete once READ is implemented
     return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
 
